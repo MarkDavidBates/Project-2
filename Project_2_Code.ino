2ab76ce87cc7e15c92da86f96561efe9ecdb05e6
MarkDavidBates <56939035+MarkDavidBates@users.noreply.github.com>
2020-04-11 11:47:34 +0100

    Add files via upload
    added code file to the repositiory

---
+++ Project_2_Code.ino
@@ -0,0 +1,46 @@
+#include <math.h>
+
+const int B = 4275;
+const int R0 = 100000;
+const int pinTempSensor = A0;
+
+const int pinAdc = A3;
+
+#if defined(ARDUINO_ARCH_AVR)
+#define debug  Serial
+#elif defined(ARDUINO_ARCH_SAMD) ||  defined(ARDUINO_ARCH_SAM)
+#define debug  SerialUSB
+#else
+#define debug  Serial
+#endif
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int a = analogRead(pinTempSensor);
+
+    float R = 1023.0/a-1.0;
+    R = R0*R;
+
+    float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
+
+    Serial.print("temperature = ");
+    Serial.println(temperature);
+
+    delay(100);
+
+    long sum = 0;
+    for(int i=0; i<32; i++)
+    {
+        sum += analogRead(pinAdc);
+    }
+
+    sum >>= 5;
+
+    Serial.println(sum);
+    delay(10);
+}
